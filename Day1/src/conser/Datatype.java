f2b34da0890af52a1f15ba10985f85e8bbb260f0
Pranathi <cherukupranathigoud@gmail.com>
2025-08-15 18:06:27 +0530

    Programming

---
+++ Day1/src/conser/Datatype.java
@@ -0,0 +1,19 @@
+package conser;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		
+		int value1 = 8 / 2; //integer division
+		float value2 = 101 / 61;
+		double value3 = 10d / 6d;
+		System.out.println("value 1=" + value1); 
+		System.out.println("value 2=" + value2);
+		System.out.println("value 3=" + value3);
+		
+		
+
+
+	}
+
+}
